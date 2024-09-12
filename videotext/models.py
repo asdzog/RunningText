ad9431ed9499c56607f90a829cc27c7a2a798f1c
asd <asdzog@mail.ru>
2024-09-12 19:49:16 +0400

    create videorequest model

--- videotext/models.py
+++ videotext/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+
+class VideoRequest(models.Model):
+
+    text = models.CharField(max_length=255, verbose_name='текст')
+    video_file = models.FileField(upload_to='videos/', verbose_name='файл')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создан')
+
+    def __str__(self):
+        return f"Video for: {self.text[:50]} (Created: {self.created_at})"
+
+    class Meta:
+        verbose_name = 'Запрос'
+        verbose_name_plural = 'Запросы'
