62cba9a379788b92dc2b6ddf585258b88a7ce727
aleksdzog <asdzog@mail.ru>
2024-09-16 14:25:56 +0400

    fix creating сyrillic filenames

--- videotext/views.py
+++ videotext/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from videotext.forms import TextForm
 from videotext.utils import generate_video
 import os
+from urllib.parse import quote
 from django.http import FileResponse, HttpResponse
 
 
@@ -13,16 +14,18 @@ def video_form(request):
             text = form.cleaned_data['text']
             try:
                 video_path = generate_video(text)
-                # Логирование пути
                 logging.info(f"Путь к видео: {video_path}")
 
                 if os.path.exists(video_path):
+                    file_name = os.path.basename(video_path)
+                    safe_filename = quote(file_name)  # Кодируем имя файла для использования в HTTP заголовках
                     response = FileResponse(open(video_path, 'rb'), content_type='video/mp4')
-                    response['Content-Disposition'] = f'attachment; filename="{os.path.basename(video_path)}"'
+                    response['Content-Disposition'] = f'attachment; filename*=UTF-8\'\'{safe_filename}'
                     return response
                 else:
                     return HttpResponse("Файл не найден", status=404)
             except Exception as e:
+                logging.error(f"Ошибка при обработке видео: {str(e)}")
                 return HttpResponse(f"Возникла ошибка: {str(e)}", status=500)
     else:
         form = TextForm()
